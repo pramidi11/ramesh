342a1788e11f9c80c0cef8b1997c0186ef771371
pradeep ramidi <pradeep.ramidi@team.telstra.com>
2021-11-11 09:06:47 +1100

    random

--- src/main/java/logbook/Task.java
+++ src/main/java/logbook/Task.java
@@ -1,6 +1,8 @@
 package logbook;
 
 import java.time.ZonedDateTime;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
@@ -16,6 +18,15 @@ public class Task {
     public String addProperty3="";
     public boolean success=false;
 
+    public String getRandom(String temp) {
+
+        String[] ranges = temp.split(";");
+        Random r = new Random();
+        double random = (r.nextInt(21)-10) / 10.0;
+
+        int number = r.nextInt(Integer.parseInt(ranges[1]) - Integer.parseInt(ranges[0])) + Integer.parseInt(ranges[0]);
+        return String.valueOf(random+ (double) number);
+    }
 
     public boolean isSuccess() {
         return success;
@@ -57,6 +68,9 @@ public class Task {
     }
 
     public String getAddProperty1() {
+        if(addProperty1.contains(";")) {
+            return getRandom(addProperty1);
+        }
         return addProperty1;
     }
 
